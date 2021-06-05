5f522d139f09a41544f3ba026110a009efc62dfa
MarinF00 <marinchoooooo@gmail.com>
2021-06-05 17:38:00 +0300

    Some css

--- FenOLX/views/site/html/itemView.php
+++ FenOLX/views/site/html/itemView.php
@@ -1,16 +1,24 @@
 <?php
 
 echo "
+<b>
 <h1>$data->item</h1>
-<p>$data->description</p>
-<p>$data->gender</p>
-<p>$data->price</p>
-<p>$data->razmer</p>
-<p>$data->contact</p>
-<p><img width='300px' src=".IMG_PATH."$data->thumbnail></p>
+<label>Description:</label>
+$data->description<br>
+<label>Gender:</label>
+$data->gender<br>
+<label>Price:</label>
+$data->price<br>
+<label>Size:</label>
+$data->razmer<br>
+<label>Contacts:</label>
+$data->contact<br>
+<label>Thumbnail</label><br>
+<img width='300px' src=".IMG_PATH."$data->thumbnail>
 <form action='" . APPLICATION_PATH . "index.php?controller=items&action=update&item_id=$data->id' method='post'>
     <button type='submit'>Update</button>
 </form>
+</b>
 
 
 ";
@@ -21,8 +29,10 @@ echo "
         background-repeat: no-repeat;
         background-size: 100% 100%;
         background-attachment: fixed;
-        background-image: url('https://live.staticflickr.com/2877/33823450815_411667305b_b.jpg');
-    //}
+        background-image: url('https://pbs.twimg.com/media/D49aN5YX4AArKYo.jpg');
+        font-size: 20px;
+        color:white;
+    }
 
     p {
         color:white;
@@ -30,5 +40,8 @@ echo "
     h1 {
         color:white;
     }
-</style>";
+
+
+
+</style>
 
