d1ed7f60f2eccc0eec8d77db72b68624345ca9b7
MarinF00 <marinchoooooo@gmail.com>
2021-06-10 21:43:21 +0300

    Removed Welcome role

--- FenOLX/views/site/welcome.php
+++ FenOLX/views/site/welcome.php
@@ -4,7 +4,6 @@ require_once "navigation.php";
 
     <header>
         <p>Welcome <?php if (!empty($_SESSION) && !empty($_SESSION["full_name"])) echo $_SESSION["full_name"];?></p>
-        <p>Welcome <?php if (!empty($_SESSION) && !empty($_SESSION["full_name"])) echo $_SESSION["user_role"];?></p>
     </header>
 
 <body>
